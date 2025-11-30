0c5f70903a9c8fbc70b3de9d5b46943fbb5cbb30
Sanjay-R <sanjayrangrej6@gmail.com>
2025-11-30 14:47:37 +0530

    Initial commit: static site in public/

--- api/index.py
+++
@@ -1,8 +0,0 @@
-import os, sys, pathlib
-sys.path.insert(0, str(pathlib.Path(__file__).parent.parent))
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
-os.environ['DJANGO_ALLOW_ASYNC_UNSAFE'] = 'true'
-
-from django.core.wsgi import get_wsgi_application
-app = get_wsgi_application()
--- build_files.py
+++
@@ -1,8 +0,0 @@
-import os, django
-from django.conf import settings
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
-django.setup()
-
-from django.core.management import call_command
-call_command('collectstatic', '--noinput', '--clear')
